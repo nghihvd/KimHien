9fa4644c9a8db6b12d507b3a797bbc97599eb5f9
Đông Nghi <82652596+nghihvd@users.noreply.github.com>
2024-12-04 18:08:26 +0700

    db

--- KimHien/KimHien/Program.cs
+++ KimHien/KimHien/Program.cs
@@ -1,7 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+/**builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseMySql(
+    builder.Configuration.GetConnectionString("DefaultConnection"),
+    new MySqlServerVersion(new Version(8, 0, 32)) // Thay bằng version của MySQL bạn dùng
+));
+**/
 
 var app = builder.Build();
 
